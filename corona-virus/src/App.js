4e6406a54a0cebfa8958d595daae0c800bd79c20
murtaza <murtazahassani105@gmail.com>
2020-04-17 00:17:17 +0300

    Corona Virus Dashboard

--- corona-virus/src/App.js
+++ corona-virus/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Axios from 'axios';
 import './index.css';
 export default class App extends React.Component{
+     constructor(props){
+          super(props);
+          this.getCountryData = this.getCountryData.bind(this);
+     }
      state = {
           confirmed: 0,
           recovered: 0,
@@ -15,8 +19,10 @@ export default class App extends React.Component{
      async getData() {
           const resApi = await Axios.get("https://covid19.mathdro.id/api");
           const resCountries = await Axios.get('https://covid19.mathdro.id/api/countries');
-     
-          const countries = Object.keys(resCountries.data.countries);
+          const countries = []; 
+          for(var i = 0; i <resCountries.data.countries.length; i++){
+         countries.push(resCountries.data.countries[i].name);
+          }
           this.setState({
                confirmed: resApi.data.confirmed.value,
                recovered: resApi.data.recovered.value,
@@ -24,30 +30,65 @@ export default class App extends React.Component{
                countries
           });
      }
-     renderCountryOptions() {
 
+     async getCountryData(e) {
+          if(e.target.value === "Worldwide"){
+          return this.getData();
+          }
+          try {
+          const res = await Axios.get(`https://covid19.mathdro.id/api/countries/${e.target.value}`);
+          this.setState({
+               confirmed: res.data.confirmed.value,
+               recovered: res.data.recovered.value,
+               deaths: res.data.deaths.value
+          })
+     }
+     catch (err) {
+          if(err.response.status === 404)
+          this.setState({
+          confirmed: "no data available",
+          recovered: "no data available",
+          deaths: "no data available"
+          });
+     }
+     }
+
+     renderCountryOptions() {
+          return this.state.countries.map((countries, i) => {
+               return <option key={i}>{countries}</option>
+          })
      }
      render() {
           return (
           <div className="container">
+
                <h1>Corona Update</h1>
-               <select>
+               <br />
+               <h1>به روزرسانی کرونا</h1>
+               <select className="dropdown" onChange={this.getCountryData}>
+                    <option>Worldwide</option>
                   {this.renderCountryOptions()}
                </select>
 
                <div className="flex">
                <div className="box confirmed">
-                    <h3>Confirmed cases</h3>
+                    <h3>Confirmed cases</h3> 
+                    <br />
+                    <h3>موارد تایید شده</h3>
           <h4>{this.state.confirmed}</h4>
                </div>
               <div className="box recovered">
                <h3>Recovered cases</h3>
+               <br />
+               <h3>موارد بهبود یافته</h3>
              
           <h4>{this.state.recovered}</h4>
                </div>
 
                <div className="box deaths">
                <h3>Deaths</h3>
+               <br />
+               <h3>موارد فوت شده</h3>
           <h4>{this.state.deaths}</h4>
                </div>
           </div>
